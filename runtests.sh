b6e7ebba29d3ac04cc01ce3e4a406a90625c3d31
Nils Ohlmeier <nils@mozilla.com>
2015-01-20 12:14:13 -0800

    activate python environment before test execution

--- runtests.sh
+++ runtests.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -x
 
+source ~/venv/bin/activate
+
 export DISPLAY=:0
 export LOOP_SERVER=$WORKSPACE/loop-server
 #export LOOP_SERVER=/home/mozilla/loop-server
 export STANDALONE_SERVER=$WORKSPACE/loop-standalone
 #export STANDALONE_SERVER=/home/mozilla/loop-standalone
 
+cd $WORKSPACE
 python $WORKSPACE/marionette/marionette/runtests.py --type=browser --binary=$WORKSPACE/firefox/firefox $WORKSPACE/marionette/tests/browser/components/loop/test/functional/manifest.ini
